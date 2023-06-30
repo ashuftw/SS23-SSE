b102e2a3b89490ec7363006503a7b7caa30ef8dc
ashuftw <psashuthosh@gmail.com>
2023-06-30 21:18:13 +0200

    230628 task-3

--- assignment-I/model/triangle.py
+++ assignment-I/model/triangle.py
@@ -15,22 +15,6 @@ class Triangle(Shape):
     # TODO: Task 4 - Implement the scale method
 
     def draw(self, painter: QPainter) -> None:
-        # get point info from calc_points method
-        # define Qpoints
-        x1, x2, x3, y1, y2, y3 = self.calc_points()
-        points = [QPoint(x1, y1), QPoint(x2, y2), QPoint(x3, y3)]
+        # TODO: Task 3 - Implement the drawing mechanism for a triangle
+        pass
 
-        painter.drawPolygon(points)
-        painter.drawText(self._center.x - 2, self._center.y, str(self._id))
-
-    def calc_points(self):
-        # define the coordinates of the triangle's points based on center and side length
-        beta = 30 * math.pi / 180
-        x1 = self._center.x - (2 / 3) * self._side * math.cos(beta)  2
-        x2 = self._center.x
-        x3 = self._center.x + (2 / 3) * self._side * math.cos(beta)  2
-
-        y1 = y3 = self._center.y + (1 / 3) * self._side * math.cos(beta)
-        y2 = self._center.y - (2 / 3) * self._side * math.cos(beta)
-
-        return x1, x2, x3, y1, y2, y3
